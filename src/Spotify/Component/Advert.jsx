490390353614e80269c90cdaa3e1cb0974ea0118
byungil712 <jam1598@naver.com>
2026-03-31 15:44:20 +0900

    20260331-1

--- src/Spotify/Component/Advert.jsx
+++ src/Spotify/Component/Advert.jsx
@@ -8,7 +8,7 @@ const Advert = () => {
       <div className="advart">
          <div className="advart_i">
             <div className="advart_left">
-               <span>spotify 미리 듣기</span>
+               <span>Spotify 미리 듣기</span>
                <p>
                   가끔 표시되는 광고와 함께 무제한 곡 및 팟캐스트를 이용하려면
                   가입하세요. 신용카드는 필요 없습니다.
